471363678abc70c8a556f16894c6c1e93b05dfa6
Muhammed <107894012+Bidz42@users.noreply.github.com>
2022-08-29 09:11:47 +0100

    MERN stack app frontend

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Home.css";
+
+function Home() {
+    return (
+        <Row>
+            <Col md={6} className="d-flex flex-direction-column align-items-center justify-content-center">
+                <div>
+                    <h1>Share whats on your mind with your friends</h1>
+                    <p>Iron Chat keeps you connected</p>
+                    <LinkContainer to="/chat">
+                        <Button variant="success">
+                            Lets Go!!! <i className="fas fa-comments home-message-icon"></i>
+                        </Button>
+                    </LinkContainer>
+                </div>
+            </Col>
+            <Col md={6} className="home__bg"></Col>
+        </Row>
+    );
+}
+
+export default Home;
